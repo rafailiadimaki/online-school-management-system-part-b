a5632c33f939894a278f80bfb5c089896e63cbdd
rafailiadimaki <80916640+rafailiadimaki@users.noreply.github.com>
2021-08-22 13:48:13 +0300

    Delete 5. all the students per course.sql

--- 5. all the students per course.sql
+++
@@ -1,3 +0,0 @@
-SELECT students.stud_id, first_name, last_name, title 
-FROM students, courseregistrations,courses
-WHERE students.stud_id = courseregistrations.stud_id AND courses.course_id = courseregistrations.course_id;
