c2ec4da088c252e3ff6384038fbd56b0d036e620
rafailiadimaki <80916640+rafailiadimaki@users.noreply.github.com>
2021-08-22 13:46:23 +0300

    Delete 5. All the assignments per course.sql

--- 5. All the assignments per course.sql
+++
@@ -1,4 +0,0 @@
-SELECT courses.title, assignments.title 
-FROM assignments, coursework, courses
-WHERE assignments.assignments_id = coursework.assignments_id  AND courses.course_id = coursework.course_id
-ORDER BY courses.title;
