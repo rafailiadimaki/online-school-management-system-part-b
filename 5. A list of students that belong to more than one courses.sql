466edd420fd3d534525ce75a0ff6a08f12ecd50d
rafailiadimaki <80916640+rafailiadimaki@users.noreply.github.com>
2021-08-22 13:45:46 +0300

    Delete 5. A list of students that belong to more than one courses.sql

--- 5. A list of students that belong to more than one courses.sql
+++
@@ -1,6 +0,0 @@
-SELECT stud_id,first_name, last_name
-FROM students
-WHERE students.stud_id IN (SELECT courseregistrations.stud_id
-FROM courseregistrations
-GROUP BY courseregistrations.stud_id
-HAVING COUNT(*) > 1);
